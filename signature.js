7264682f80b3e889419c97c35a763f6fbb3e2e49
Your Name <pranshu@tersesoft.com>
2019-07-08 13:00:06 +0530

    Private key exposed fixed {.env created}

--- signature.js
+++ signature.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Tx = require('ethereumjs-tx').Transaction
 const Web3 = require('web3')
 const web3 = new Web3('https://ropsten.infura.io/v3/7bc3250b95194b04b89d7ca9284d4e5a')
@@ -6,21 +7,13 @@ const account1 = "0x1c47ab2738e07114450F8a1B58a5DaFed1c01026"
 const account2 = "0x909c183ce8F35634e94b899C6Ae96243cc3fD976"
 
 const privateKey1 = Buffer.from(
-    'DF4D030ABEF8A8978D3769580C003AC0F4BBFAD8C45A68930ACE2842307E6F7D',
+    process.env.PRIV_KEY1,
     'hex'
   )
-
 const privateKey2 = Buffer.from(
-'20264AB8A8922BEB9A70CF9E61B011328E7A7BC9BF2F0101A4BA38BAE1970E9D',
+ process.env.PRIV_KEY1,
 'hex'
 )
-
-// const privateKey1 = 'DF4D030ABEF8A8978D3769580C003AC0F4BBFAD8C45A68930ACE2842307E6F7D'
-// const privateKey2 = '20264AB8A8922BEB9A70CF9E61B011328E7A7BC9BF2F0101A4BA38BAE1970E9D'
-
-
-console.log("Block")
-
 
 
 web3.eth.getTransactionCount(account1,(err,txCount) =>{
@@ -30,7 +23,7 @@ web3.eth.getTransactionCount(account1,(err,txCount) =>{
     const txObject = {
         nonce:web3.utils.toHex(txCount),
         to:account2,
-        value:web3.utils.toHex(web3.utils.toWei('0.1','ether')),
+        value:web3.utils.toHex(web3.utils.toWei('0.002','ether')),
         gasLimit:web3.utils.toHex(21000),
         gasPrice:web3.utils.toHex(web3.utils.toWei('10','gwei'))
     }
